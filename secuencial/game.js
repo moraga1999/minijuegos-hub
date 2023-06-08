a0d98855d8572e4cf1dca95b725278eeefbde452
moraga1999 <javier_moragav@hotmail.com>
2023-06-08 13:30:10 -0400

    HU01 Escena principal
    - map.json con software Tiled
    - fix error 404 favicon.ico

--- secuencial/game.js
+++ secuencial/game.js
@@ -3,18 +3,19 @@
 function preload ()
 {
     this.load.image('tiles','assets/tileset.png')
-    this.load.tilemapTiledJSON('map','assets/map.json')
+    this.load.tilemapTiledJSON('map','map.json')
 }
 
 function create ()
 {
-    const map = this.make.tilemap({ key: 'map' });
+    const map = this.make.tilemap({ key: 'map', tileWidth: 48, tileHeight: 48 });
     // The first parameter is the name of the tileset in Tiled and the second parameter is the key
     // of the tileset image used when loading the file in preload.
     const tiles = map.addTilesetImage('tileset', 'tiles');
     // You can load a layer from the map using the layer name from Tiled, or by using the layer
     // index (0 in this case).
-    const layer = map.createLayer('background', tiles, 0, 0);
+    const background = map.createLayer('background', tiles, 0, 0);
+    const platforms = map.createLayer('platforms', tiles, 0, 0);
 }
 
 var config = {
